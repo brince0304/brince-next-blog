c44179c7e007556ad3f51e810ace4b5d2ab7a0e8
brince0304 <brince0304@gmail.com>
2024-08-18 16:10:11 +0900

    feat: 토글버튼 분리

---
+++ src/components/common/ThemeToggle/ThemeToggle.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
+import dynamic from 'next/dynamic';
+import ButtonLink from "@/components/common/LinkButton/LinkButton";
+
+const ThemeToggle = () => {
+    const { theme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return null;
+    }
+
+    return (
+        <ButtonLink
+            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+        >
+            {theme === 'dark' ? '🌙' : '🌞'}
+        </ButtonLink>
+    );
+};
+
+export default dynamic(() => Promise.resolve(ThemeToggle), { ssr: false });
