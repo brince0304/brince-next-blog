456841872e00ae53c83ef754b8ac4814fa9b0223
brince0304 <brince0304@gmail.com>
2024-08-18 16:14:12 +0900

    feat: 로딩 컴포넌트 구현

---
+++ src/components/common/Loading/Loading.stories.tsx
@@ -0,0 +1,25 @@
+import type {Meta} from "@storybook/react";
+import Loading from "@/components/common/Loading/Loading";
+
+const meta: Meta<typeof Loading> = {
+    title: 'Components/Loading',
+    component: Loading,
+    tags: ['autodocs'],
+    argTypes: {
+        isLoading: { control: 'boolean' },
+    },
+};
+
+export default meta;
+
+export const Default = {
+    args: {
+        isLoading: true,
+    },
+};
+
+export const NotLoading = {
+    args: {
+        isLoading: false,
+    },
+};
